c65b234edc592253dc601aacd9fe7f4e2b67de9a
geekbrains <gang32rus@mail.ru>
2021-05-20 13:25:46 +0300

    Изменение в первых двух заданиях

--- src/main/java/Homework1/Main.java
+++ src/main/java/Homework1/Main.java
@@ -1,32 +1,41 @@
 package Homework1;
 
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        Integer[] integers = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        String[] strings = {"ноль", "один", "два", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять", "десять"};
 
-        BoxMassive<Integer> firstBox = new BoxMassive<>(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-        BoxMassive<String> SecondBox = new BoxMassive<>("один", "два", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять", "десять");
-        firstBox.reverseElementMassive(3, 5);
-        SecondBox.reverseElementMassive(4,6);
+        List<Integer> integerList = new ArrayList<>();
+        List<String> stringList = new ArrayList<>();
 
-        List<String> stringList =  SecondBox.convertToCollection();
-        List<Integer> integerList = firstBox.convertToCollection();
+        reverse(3, 7, integers);
+        reverse(5, 10, strings);
 
-        System.out.println(stringList);
+        convertToCollection(integerList, integers);
+        convertToCollection(stringList, strings);
+
         System.out.println(integerList);
-
-
-
-
-
-
-
-
+        System.out.println(stringList);
     }
 
+    @SafeVarargs
+    private static <T> void reverse(int a, int b, T... arr) {
+        System.out.println(Arrays.toString(arr));
+        T first = arr[a];
+        arr[a] = arr[b];
+        arr[b] = first;
+        System.out.println(Arrays.toString(arr));
+        System.out.println();
+    }
+
+    @SafeVarargs
+    private static <T> void  convertToCollection(List<T> list, T... arr) {
+       list.addAll(Arrays.asList(arr));
+    }
 
 }
-//1. Написать метод, который меняет два элемента массива местами.(массив может быть любого ссылочного типа);
-//2. Написать метод, который преобразует массив в ArrayList;
